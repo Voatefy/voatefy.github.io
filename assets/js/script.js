9bc042c3845e02cc77a5441240f99c9fe4dff841
Voatefy <tefyf3356@gmail.com>
2025-05-30 23:04:20 +0300

    First commit , (Home and about done)

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+   strings: ['System Administrator', 'Network Administrator', 'Passionate about Cybersecurity', 'Skilled in Linux and Windows Server'],
+   typeSpeed:40,
+   backSpeed:40,
+   backDelay:1000,
+   loop:true
+});
+
